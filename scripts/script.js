bf8e76e1950888f1c0c0bd2728eecc66b060b21b
EkaterinaRechkina <73222009+EkaterinaRechkina@users.noreply.github.com>
2021-07-25 12:53:24 +0300

    JS fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,38 @@
+'use strict'
 let openPopup = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close');
-
-openPopup.addEventListener('click', function() {
-    popup.classList.add('popup_opened');
-}) 
-
-closePopup.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
-})
-
-
-let formElement = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__user');
-let jobInput = document.querySelector('.popup__job');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__job');
 
+
+function open() {
+    popup.classList.add('popup_opened');
+}
+function close() {
+    popup.classList.remove('popup_opened'); 
+}
+openPopup.addEventListener('click', open); 
+
+closePopup.addEventListener('click', close);
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_edit_name');
+let jobInput = document.querySelector('.popup__input_edit_job');
+
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    let NameUser = nameInput.value;
+    let AboutUser = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+    profileName.textContent = NameUser;
+    profileDescription.textContent = AboutUser;
+   close()
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
