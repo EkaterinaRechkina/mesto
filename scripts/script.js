ffed36cca56097588c95db9d8a4c68b62c55bd27
EkaterinaRechkina <73222009+EkaterinaRechkina@users.noreply.github.com>
2021-08-10 06:48:27 +0300

    5 project v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 'use strict'
+//userPopup
 let openPopup = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close');
@@ -7,12 +8,19 @@ let profileDescription = document.querySelector('.profile__job');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_edit_name');
 let jobInput = document.querySelector('.popup__input_edit_job');
+//cardPopup
+const popupCard = document.querySelector('#card');
+const cardAdd = document.querySelector('.profile__button-add');
+const closeCardPopup = document.querySelector('#close-card');
+const cardName = document.querySelector('.popup__input_card-name');
+const cardImg = document.querySelector('.popup__input_card-img');
 
+
+//popup
 function open() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
     popup.classList.add('popup_opened');
-    
 }
 function close() {
     popup.classList.remove('popup_opened'); 
@@ -20,21 +28,111 @@ function close() {
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-
-    // Получите значение полей jobInput и nameInput из свойства value
     let NameUser = nameInput.value;
     let AboutUser = jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
     profileName.textContent = NameUser;
     profileDescription.textContent = AboutUser;
    close()
 }
 
 openPopup.addEventListener('click', open); 
-
 closePopup.addEventListener('click', close);
-
 formElement.addEventListener('submit', formSubmitHandler);
 
+function openCard() {
+    popupCard.classList.add('popup_opened');
+}
+function closeCard() {
+    popupCard.classList.remove('popup_opened'); 
+}
+
+cardAdd.addEventListener('click', openCard);
+closeCardPopup.addEventListener('click', closeCard);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    let NameUser = nameInput.value;
+    let AboutUser = jobInput.value;
+    profileName.textContent = NameUser;
+    profileDescription.textContent = AboutUser;
+   close()
+}
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+const initialCards = [
+    {
+      title: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      title: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      title: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      title: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      title: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      title: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const cards = document.querySelector('.cards');
+
+//удаление карточки
+function deleteCard(evt) {
+    const card = evt.target.closest('.card');
+    cards.removeChild(card)
+  }
+
+  //добавить карточки на страницу
+
+function addCard(item) {
+    const elementTemplate = document.querySelector('#card-template').content;
+    const card = elementTemplate.querySelector('.card').cloneNode(true);
+    
+    card.querySelector('.card__title').textContent = item.title;
+    card.querySelector('.card__image').src =item.link;
+    cards.prepend(card);
+
+    card.querySelector('.card__trash').addEventListener('click', deleteCard)
+    card.querySelector('.card__button-like').addEventListener('click', evt => {
+      evt.target.classList.toggle('card__button-like_active')
+    })
+    cards.prepend(card)
+  }
+
+  initialCards.forEach(addCard);
+
+
+  //Загрузка карточек
+
+  
+    const addCardNameInput = popupAdd.querySelector('.popup__input_card-name');
+    const addCardLinkInput = popupAdd.querySelector('.popup__input_card-img');
+    const addCardNameMain = popupAdd.querySelector('.card__description');
+    const addCardLinkMain = popupAdd.querySelector('.card__image');
+
+function addCardFromPopup(evt) {
+    evt.preventDefault()
+  
+    let cardName = addCardNameInput.value;
+    let cardLink = addCardLinkInput.value;
+    addCardNameMain.textContent = cardName;
+    addCardLinkMain.textContent = cardLink;
+
+    closeCard()
+  }
+  
+  cardAddForm.addEventListener('submit', addCardFromPopup)
